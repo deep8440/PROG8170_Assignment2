3be8f9dc9d490d60fbcb8ad9ce2dc1eae354d616
deep8440 <dpradadiya5@gmail.com>
2023-02-09 16:03:28 -0500

    Add project files.

---
+++ PROG8170_Assignment2/Circle.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PROG8170_Assignment2
+{
+
+    public class Circle
+    {
+        private int pRadius;
+
+        /// <summary>
+        /// Circle counstructor
+        /// </summary>
+        public Circle()
+        {
+            pRadius = 1;
+        }
+
+        /// <summary>
+        /// Circle counstructor
+        /// </summary>
+        /// <param name="inputRadius">The Radius.</param>
+        public Circle(int inputRadius)
+        {
+            pRadius = inputRadius;
+        }
+
+        /// <summary>
+        /// Get Radius
+        /// </summary>
+        /// <returns>The Radius</returns>
+        public int GetRadius()
+        {
+            return pRadius;
+        }
+
+        /// <summary>
+        /// Set Radius
+        /// </summary>
+        /// <param name="radius">The Radius</param>
+        public void SetRadius(int radius)
+        {
+            pRadius =radius;
+        }
+
+        /// <summary>
+        /// Get Circumference
+        /// </summary>
+        /// <returns>The Circumference Of Circle</returns>
+        public double GetCircumference()
+        {
+            return 2 * Math.PI * pRadius;
+        }
+        /// <summary>
+        /// Get Area
+        /// </summary>
+        /// <returns>The Area Of Circle</returns>
+        public double GetArea()
+        {
+            return (Math.PI * pRadius * pRadius);
+        }
+    }
+}
---
+++ PROG8170_Assignment2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PROG8170_Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
