41632d647a91c6d38a96bd9df2dec11e36b93496
deep8440 <dpradadiya5@gmail.com>
2023-02-09 16:25:51 -0500

    nunit project added

---
+++ PROG8170_Assignment2.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace PROG8170_Assignment2.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
